f773c8794c3588fa6707479e13365e9593948adc
Brett Tofel <btofel@redhat.com>
2021-01-08 11:16:21 -0500

    Move to quarkus 1.10.5.Final
    Change CustomResourceDefinition usage for this as well

--- src/main/java/com/coreos/operator/quarkus/ClientProvider.java
+++ src/main/java/com/coreos/operator/quarkus/ClientProvider.java
@@ -1,6 +1,6 @@
 package com.coreos.operator.quarkus;
 
-import io.fabric8.kubernetes.api.model.apiextensions.CustomResourceDefinition;
+import io.fabric8.kubernetes.api.model.apiextensions.v1beta1.CustomResourceDefinition;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.dsl.NonNamespaceOperation;
